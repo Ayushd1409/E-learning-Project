db6e989bcb79cbd3a04b32855118dddfa17a66bb
Astitva Pathak <astitvapathak2711@gmail.com>
2024-08-27 19:24:45 +0530

    .

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -9,7 +9,7 @@ router.post("/course/new",isAuth, isAdmin, uploadFiles, createCourse);
 router.post("/course/:id", isAuth, isAdmin, uploadFiles, addLectures);
 router.delete("/course/:id", isAuth, isAdmin, deleteCourse);
 router.delete("/lecture/:id",isAuth, isAdmin, deleteLecture);
-router.get('/stats', isAuth, isAuth, getAllStats);
+router.get('/stats', isAuth, getAllStats);
 router.put("/user/:id", isAuth, isAdmin, updateRole);
 router.get("/users", isAuth, isAdmin, getAllUser);
 
